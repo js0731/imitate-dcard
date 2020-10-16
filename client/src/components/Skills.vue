9a71f2abfbfd9304f96fe65a2a0aa7b6a6538d3f
js0731 <a0905050792@gmail.com>
2020-10-16 13:09:38 +0800

    edit skill component

--- client/src/components/Skills.vue
+++ client/src/components/Skills.vue
@@ -1,5 +1,7 @@
 <template>
-  <section class="skills">skills</section>
+  <section class="skills">
+    <h2>Skills</h2>
+  </section>
 </template>
 
 <script>
@@ -9,9 +11,15 @@
 .skills {
   width: 100%;
   height: 100vh;
-  background: url("../assets/jpg/bg-masthead.jpg");
   background-attachment: fixed; /* 固定圖像 */
   background-position: center center; /* 圖像置中 */
   background-size: cover;
+  background: #353b48;
+  h2 {
+    font-weight: 600;
+    font-size: 2rem;
+    text-align: center;
+    color: #ffffff;
+  }
 }
 </style>
