b7a01bdd15d953052113daa3310723de60599207
js0731 <a0905050792@gmail.com>
2020-10-20 22:57:35 +0800

    edit About component

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,14 +1,35 @@
 <template>
-  <section class="about">about</section>
+  <section class="about">
+    <h2 class="title">About</h2>
+    <p class="content">
+      你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容你好我是李冠容
+    </p>
+  </section>
 </template>
 
 <script>
+export default {
+  name: "about",
+};
 </script>
 
 <style lang="scss" scoped>
 .about {
   width: 100%;
-  height: 600px;
-  background: #ffffff;
+  padding: 30px 0;
+  background: #3c6382;
+  .title {
+    font-weight: 600;
+    font-size: 2rem;
+    text-align: center;
+    color: #ffffff;
+    margin-bottom: 20px;
+  }
+  .content {
+    max-width: 500px;
+    font-size: 1.2rem;
+    margin: 0 auto;
+    text-align: center;
+  }
 }
 </style>
